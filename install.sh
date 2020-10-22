d919d0bdf13c6ea7a41b0929a5516b8598cb2857
Brian Shu <littlebubu.shu@gmail.com>
2020-10-21 20:31:55 -0400

    added prompt and trap

--- install.sh
+++ install.sh
@@ -2,35 +2,58 @@
 
 PROGNAME="$(basename "$0")"
 DATE=$(date +'%Y')
+NEWDIR="$HOME/wpilib/$DATE"
+TARFILE=wpilib.tar.gz
 
 error() {
-    echo "${PROGNAME}: ${1:-"Unkown Error"}" 1>&2
+    printf "${PROGNAME}: ${1:-"Unkown Error"}\n" 1>&2
     exit 1
 }
 
+cleanup() {
+    rm -f $TARFILE
+}
+
+interrupt() {
+    printf "\nInterrupted\n"
+    cleanup
+}
+
+trap cleanup EXIT
+trap interrupt SIGINT
+
+if [ -d "$NEWDIR" ]; then
+    error "$NEWDIR exists. Please remove it to prevent this script from overriding the directory"
+fi
+
 cd ~/Downloads
 
-echo "Installing wpilib archive file"
-curl -fLo wpilib.tar.gz https://github.com/wpilibsuite/allwpilib/releases/download/v2020.3.2/WPILib_Linux-2020.3.2.tar.gz > /dev/null 2>&1 ||
+while true; do
+    read -p "Warning, wpilib will take a while to install. Continue? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n "
+    esac
+done
+
+printf "\nDownloading wpilib from github\n\n"
+curl --progress-bar -Lo $TARFILE https://github.com/wpilibsuite/allwpilib/releases/download/v2020.3.2/WPILib_Linux-2020.3.2.tar.gz ||
     error "Failed to download wpilib archive file from github releases"
 
-echo "Extracting wpilib archive file"
-tar -xzf wpilib.tar.gz || error "Failed to extract wpilib tar file"
+printf "Extracting wpilib archive file\n\n"
+tar -xzf $TARFILE || error "Failed to extract wpilib tar file"
 
-echo "Removing leftovers"
-rm wpilib.tar.gz
-
-echo "Moving stuff to the correct place"
-mkdir -p "~/wpilib/$DATE"
-mv wpilib/* "~/wpilib/$DATE"
+mkdir -p "$NEWDIR"
+mv wpilib/* "$NEWDIR"
 rmdir wpilib
 
-echo "Running ToolsUpdater.py"
-cd "~/wpilib/$DATE"
-python3 tools/ToolsUpdater.py > /dev/null 2>&1 || error "Failed to run python updator script"
+printf "Running ToolsUpdater.py\n\n"
+cd "$HOME/wpilib/$DATE"
+python3 tools/ToolsUpdater.py || error "Failed to run python updator script"
 
-echo "Installing vscode extensions"
+printf "Installing vscode extensions\n\n"
 for filename in vsCodeExtensions/*; do
-    echo "Installing extension $filename"
-    code --install-extension "$filename" > /dev/null 2>&1 || error "Failed to install extension $filename"
+    printf "Installing extension $filename\n\n"
+    code --install-extension "$filename\n\n" || error "Failed to install extension $filename"
 done
